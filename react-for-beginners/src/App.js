f8e41325b9d420d0b07b9a2b247962ce0368ef05
hanjy-slowturtle <h920915@gmail.com>
2022-07-31 14:56:48 +0900

    ~ #7.3 Movie App part One

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,48 +2,38 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [coinIndex, setCoinIndex] = useState(0);
-  const [usd, setUsd] = useState(0);
-  const onSelect = (event) => {
-    setCoinIndex(event.target.value);
-  };
-  const onChange = (event) => {
-    setUsd(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1>Coin ({coins.length})</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onSelect}>
-          {coins.map((coin, index) => (
-            <option key={coin.id} value={index}>
-              {coin.name} ({coin.symbol}): ${coin.quotes["USD"].price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt={movie.title} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <br />
-      USD: <input type="number" value={usd} onChange={onChange} />
-      <br />
-      Coin:{" "}
-      <input
-        type="text"
-        value={
-          coins.length > 0 ? usd / coins[coinIndex].quotes["USD"].price : 0
-        }
-        readOnly
-      />
     </div>
   );
 }
